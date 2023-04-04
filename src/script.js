cc34ff7ced1c7166a8b73135812afe0a78434b64
Testers7777 <97400362+Testers7777@users.noreply.github.com>
2023-04-04 18:36:23 +0200

    Create script.js

---
+++ src/script.js
@@ -0,0 +1,13 @@
+const c = document.getElementById('bg');
+const colors = {'st': 'white', 'nd': 'grey'};
+
+c.addEventListener("click", () => {
+  const a = document.querySelector('body');
+  if (document.getElementById('body').className == colors['nd']) {
+    a.style.setProperty("background-color", colors['st']);
+    a.setAttribute("class", colors['st']);
+  } else {
+    a.style.setProperty("background-color", colors['nd']);
+    a.setAttribute("class", colors['nd']);
+  }
+});
