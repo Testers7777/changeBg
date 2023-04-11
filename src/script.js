aec35e2d79249567251ff63d7fafa43fe6edddef
Testers7777 <97400362+Testers7777@users.noreply.github.com>
2023-04-11 14:22:55 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -3,11 +3,9 @@ const colors = {'st': 'white', 'nd': 'grey'};
 
 c.addEventListener("click", () => {
   const a = document.querySelector('body');
-  if (document.getElementById('body').className == colors.nd) {
+  if (a.style.getPropertyValue('background-color') == colors.nd) {
     a.style.setProperty("background-color", colors.st);
-    a.setAttribute("class", colors.st);
   } else {
     a.style.setProperty("background-color", colors.nd);
-    a.setAttribute("class", colors.nd);
   }
 });
